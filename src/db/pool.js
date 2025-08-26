9851332bdaa9ecbb95f609dfa956482744b190fb
Oluwatunmise Daramola <idowu.o.daramola@gmail.com>
2025-08-26 03:07:10 +0100

    correct db password

--- src/db/pool.js
+++ src/db/pool.js
@@ -6,7 +6,7 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   waitForConnections: true,
