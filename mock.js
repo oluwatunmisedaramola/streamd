530032a45f87f37d904bae83e86a4594a903ad0c
Oluwatunmise Daramola <idowu.o.daramola@gmail.com>
2025-08-29 10:56:53 +0100

    fix swaggerUI

--- mock.js
+++ mock.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import path from "path";
 import { fileURLToPath } from "url";
