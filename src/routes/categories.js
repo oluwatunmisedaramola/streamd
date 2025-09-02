af3c9a79745a040afd0dc5cb634f3fef59c827e3
Oluwatunmise Daramola <idowu.o.daramola@gmail.com>
2025-09-02 12:17:12 +0100

    add safequery to category router

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,5 +1,5 @@
 import express from "express";
-import pool from "../db/pool.js";
+import { safeQuery } from "../db/pool.js";
 import { queries } from "../db/queries.js";
 
 const router = express.Router();
@@ -7,7 +7,7 @@ const router = express.Router();
 // GET /api/categories
 router.get("/", async (req, res, next) => {
   try {
-    const [rows] = await pool.query(queries.getCategories);
+    const [rows] = await safeQuery(queries.getCategories);
     res.json({ data: rows });
   } catch (err) {
     next(err);
